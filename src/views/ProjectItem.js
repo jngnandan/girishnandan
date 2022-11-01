91f146ec36f63a0eca1ec6d5221c54f6a8c208bf
jngnandan <jngnandan@gmail.com>
2022-11-01 23:29:43 +0000

    overflow screen fixed

--- src/views/ProjectItem.js
+++ src/views/ProjectItem.js
@@ -10,7 +10,7 @@ function ProjectItem(props) {
   return (
     <Link to={`/projects/${title}`}>
     <div className="bg-gray-800 p-5 rounded-lg flex flex-col justify-center items-center">
-      <img src={image} alt={name} className='w-full h-60 object-scale-down rounded-lg' />
+      <img src={image} alt={name} className='w-full h-44 md:h-60 object-scale-down rounded-lg' />
     </div>
     </Link>
   )
