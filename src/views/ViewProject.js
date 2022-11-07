8e3a3be1a3c1d5311b3157dcab0cb8de5a38e4a3
Girish Nandan <jngnandan@gmail.com>
2022-11-07 21:46:00 +0000

    googleSearchApp Added

--- src/views/ViewProject.js
+++ src/views/ViewProject.js
@@ -6,6 +6,18 @@ import {useNavigate, useLocation} from 'react-router-dom'
 
 import AppStore from '../../src/Projects/AppStore/AppStore'
 import SimpleTodo from '../../src/Projects/SimpleTodo/SimpleTodos'
+import GoogleSearchApp from '../Projects/GoogleSearchApp/GoogleSuggestions'
+
+
+const suggestionsList = [
+  {id: 1, suggestion: 'Price of Ethereum'},
+  {id: 2, suggestion: 'Oculus Quest 2 specs'},
+  {id: 3, suggestion: 'Tesla Share Price'},
+  {id: 4, suggestion: 'Price of Ethereum today'},
+  {id: 5, suggestion: 'Latest trends in AI'},
+  {id: 6, suggestion: 'Latest trends in ML'},
+]
+
 
   const ProjectList = [
     {
@@ -38,6 +50,11 @@ function ViewProject() {
         if (path === 'SimpleTodos') {
           return <SimpleTodo />
         }
+        if (path === 'GoogleSearchApp') {
+          return <GoogleSearchApp suggestionsList={suggestionsList} />
+        }
+        
+        
       
       }
       )}
