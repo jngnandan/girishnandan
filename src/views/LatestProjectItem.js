0b2afb9ba8a7748442c6d6b8d2d788734fe36092
Girish Nandan <jngnandan@gmail.com>
2023-02-10 05:12:18 +0000

    link fixed

--- src/views/LatestProjectItem.js
+++ src/views/LatestProjectItem.js
@@ -12,13 +12,11 @@ const RedirectLink = () => {
 }
 
   return (
-    // <button onClick={RedirectLink}>
-    <Link to={{ pathname: `${url}` }} target="_blank">
+        <a href={url} target="_blank">
     <div className="bg-gray-800 p-5 rounded-lg flex flex-col justify-center items-center">
       <img src={image} alt={name} className='w-full h-44 md:h-60 object-scale-down rounded-lg' />
     </div>
-    </Link>
-    // </button>
+    </a>
   )
 }
 
